322b370a28dc2e5c9384b5e55c6081674a7ee479
apsare <matthieu.labbaye@free.fr>
2021-03-25 18:08:15 +0100

    commit 2

--- src/app/components/home/table-products/table-products-datasource.ts
+++ src/app/components/home/table-products/table-products-datasource.ts
@@ -3,36 +3,13 @@ import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { map } from 'rxjs/operators';
 import { Observable, of as observableOf, merge } from 'rxjs';
-
+import { Product } from '../../../models/product'
 // TODO: Replace this with your own data model type
-export interface TableProductsItem {
-  name: string;
-  id: number;
-}
-
 
 // TODO: replace this with real data from your application
-const EXAMPLE_DATA: TableProductsItem[] = [
-  {id: 1, name: 'Hydrogen'},
-  {id: 2, name: 'Helium'},
-  {id: 3, name: 'Lithium'},
-  {id: 4, name: 'Beryllium'},
-  {id: 5, name: 'Boron'},
-  {id: 6, name: 'Carbon'},
-  {id: 7, name: 'Nitrogen'},
-  {id: 8, name: 'Oxygen'},
-  {id: 9, name: 'Fluorine'},
-  {id: 10, name: 'Neon'},
-  {id: 11, name: 'Sodium'},
-  {id: 12, name: 'Magnesium'},
-  {id: 13, name: 'Aluminum'},
-  {id: 14, name: 'Silicon'},
-  {id: 15, name: 'Phosphorus'},
-  {id: 16, name: 'Sulfur'},
-  {id: 17, name: 'Chlorine'},
-  {id: 18, name: 'Argon'},
-  {id: 19, name: 'Potassium'},
-  {id: 20, name: 'Calcium'},
+const EXAMPLE_DATA: Product[] = [
+
+
 ];
 
 /**
@@ -40,8 +17,8 @@ const EXAMPLE_DATA: TableProductsItem[] = [
  * encapsulate all logic for fetching and manipulating the displayed data
  * (including sorting, pagination, and filtering).
  */
-export class TableProductsDataSource extends DataSource<TableProductsItem> {
-  data: TableProductsItem[] = EXAMPLE_DATA;
+export class TableProductsDataSource extends DataSource<Product> {
+  data: Product[] = EXAMPLE_DATA;
   paginator: MatPaginator | undefined;
   sort: MatSort | undefined;
 
@@ -54,7 +31,7 @@ export class TableProductsDataSource extends DataSource<TableProductsItem> {
    * the returned stream emits new items.
    * @returns A stream of the items to be rendered.
    */
-  connect(): Observable<TableProductsItem[]> {
+  connect(): Observable<Product[]> {
     if (this.paginator && this.sort) {
       // Combine everything that affects the rendered data into one update
       // stream for the data-table to consume.
@@ -77,7 +54,7 @@ export class TableProductsDataSource extends DataSource<TableProductsItem> {
    * Paginate the data (client-side). If you're using server-side pagination,
    * this would be replaced by requesting the appropriate data from the server.
    */
-  private getPagedData(data: TableProductsItem[]): TableProductsItem[] {
+  private getPagedData(data: Product[]): Product[] {
     if (this.paginator) {
       const startIndex = this.paginator.pageIndex * this.paginator.pageSize;
       return data.splice(startIndex, this.paginator.pageSize);
@@ -90,7 +67,7 @@ export class TableProductsDataSource extends DataSource<TableProductsItem> {
    * Sort the data (client-side). If you're using server-side sorting,
    * this would be replaced by requesting the appropriate data from the server.
    */
-  private getSortedData(data: TableProductsItem[]): TableProductsItem[] {
+  private getSortedData(data: Product[]): Product[] {
     if (!this.sort || !this.sort.active || this.sort.direction === '') {
       return data;
     }
@@ -99,7 +76,7 @@ export class TableProductsDataSource extends DataSource<TableProductsItem> {
       const isAsc = this.sort?.direction === 'asc';
       switch (this.sort?.active) {
         case 'name': return compare(a.name, b.name, isAsc);
-        case 'id': return compare(+a.id, +b.id, isAsc);
+        case 'id': return compare(+a.idProduct, +b.idProduct, isAsc);
         default: return 0;
       }
     });
